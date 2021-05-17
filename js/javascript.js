206aeac4014c76121ade68554ce6b28613ffdb28
unknown <ghareeb_shehata@outlook.com>
2021-05-17 02:45:40 +0200

    enhancing responsiveness and finishing the page

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,15 @@
 let questions = $(".question");
 
 questions.click((e) => {
-    questions.map((index, item) => {
-        $(`#${item.nextElementSibling.id}`).removeClass("show")
-    })
-    $(`#${e.target.nextElementSibling.id}`).toggleClass("show")
+    if ($(`#${e.target.nextElementSibling.id}`).hasClass("show")) {
+        questions.map((index, item) => {
+            $(`#${item.nextElementSibling.id}`).removeClass("show")
+        })
+    } else {
+        questions.map((index, item) => {
+            $(`#${item.nextElementSibling.id}`).removeClass("show")
+        })
+        $(`#${e.target.nextElementSibling.id}`).addClass("show")
+    }
+
 })
